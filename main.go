09125aa36e7b299cdf1b9e74ba9a8e0560cd1efa
twinbird <ixa2063@gmail.com>
2025-02-10 21:25:10 +0900

    add csv export feature

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"context"
 	"database/sql"
+	"encoding/csv"
 	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/microsoft/go-mssqldb"
 )
@@ -41,7 +43,14 @@ func main() {
 		log.Fatal("getTables Failed", err)
 	}
 
-	fmt.Printf("%v\n", tables)
+	for _, table := range tables {
+		err := exportTableToCSV(db, schema, table)
+		if err != nil {
+			log.Printf("テーブル %s のエクスポートに失敗: %v\n", table, err)
+		} else {
+			fmt.Printf("テーブル %s をエクスポートしました\n", table)
+		}
+	}
 }
 
 func getTables(db *sql.DB, schema string) ([]string, error) {
@@ -73,4 +82,66 @@ func getTables(db *sql.DB, schema string) ([]string, error) {
 		tables = append(tables, tname)
 	}
 	return tables, nil
+}
+
+// テーブルの内容をCSVファイルに書き出す
+func exportTableToCSV(db *sql.DB, schema, table string) error {
+	// クエリを作成
+	query := fmt.Sprintf("SELECT * FROM [%s].[%s]", schema, table)
+	rows, err := db.Query(query)
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+
+	// カラム情報を取得
+	columns, err := rows.Columns()
+	if err != nil {
+		return err
+	}
+
+	// CSVファイルを作成
+	fileName := fmt.Sprintf("%s.csv", table)
+	file, err := os.Create(fileName)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	// カラム名を書き込む
+	if err := writer.Write(columns); err != nil {
+		return err
+	}
+
+	// 各行のデータを書き込む
+	values := make([]interface{}, len(columns))
+	valuePtrs := make([]interface{}, len(columns))
+
+	for i := range values {
+		valuePtrs[i] = &values[i]
+	}
+
+	for rows.Next() {
+		if err := rows.Scan(valuePtrs...); err != nil {
+			return err
+		}
+
+		var record []string
+		for _, val := range values {
+			if val == nil {
+				record = append(record, "NULL")
+			} else {
+				record = append(record, fmt.Sprintf("%v", val))
+			}
+		}
+
+		if err := writer.Write(record); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
