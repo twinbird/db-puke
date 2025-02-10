e3ec4341e829920947705b20598612b71c34465e
twinbird <ixa2063@gmail.com>
2025-02-10 22:28:32 +0900

    add option flags

--- main.go
+++ main.go
@@ -4,25 +4,66 @@ import (
 	"context"
 	"database/sql"
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 
 	_ "github.com/microsoft/go-mssqldb"
 )
 
+type Config struct {
+	DBType   string
+	Host     string
+	Port     int
+	Database string
+	Schema   string
+	User     string
+	Password string
+	OutDir   string
+}
+
+func parseArgs() *Config {
+	config := &Config{}
+
+	// フラグの定義
+	flag.StringVar(&config.DBType, "type", "mssql", "database type (mssql)")
+	flag.StringVar(&config.Host, "h", "localhost", "hostname")
+	flag.IntVar(&config.Port, "p", 1433, "port")
+	flag.StringVar(&config.Database, "d", "", "database")
+	flag.StringVar(&config.Schema, "s", "dbo", "schema")
+	flag.StringVar(&config.User, "u", "", "username")
+	flag.StringVar(&config.Password, "P", "", "password")
+	flag.StringVar(&config.OutDir, "o", "./", "export dir")
+
+	// パース
+	flag.Parse()
+
+	// 必須項目チェック
+	if config.Database == "" {
+		fmt.Println("エラー: データベース名 (-d) を指定してください")
+		os.Exit(1)
+	}
+	if config.User == "" {
+		fmt.Println("エラー: ユーザー名 (-u) を指定してください")
+		os.Exit(1)
+	}
+	if config.Password == "" {
+		fmt.Println("エラー: パスワード (-P) を指定してください")
+		os.Exit(1)
+	}
+
+	return config
+}
+
 func main() {
-	// SQL Server の接続情報
-	server := "localhost"
-	port := 1433
-	user := "SA"
-	password := "saPassword1234"
-	database := "dummy_database"
-	schema := "dummy_schema"
+	// オプション解析
+	config := parseArgs()
 
 	// 接続文字列の作成
 	connString := fmt.Sprintf("sqlserver://%s:%s@%s:%d?database=%s&encrypt=disable",
-		user, password, server, port, database)
+		config.User, config.Password, config.Host, config.Port, config.Database)
 
 	// データベース接続
 	db, err := sql.Open("sqlserver", connString)
@@ -38,13 +79,13 @@ func main() {
 	}
 	fmt.Println("SQL Server に接続しました。")
 
-	tables, err := getTables(db, schema)
+	tables, err := getTables(db, config.Schema)
 	if err != nil {
 		log.Fatal("getTables Failed", err)
 	}
 
 	for _, table := range tables {
-		err := exportTableToCSV(db, schema, table)
+		err := exportTableToCSV(db, config.Schema, table, config.OutDir)
 		if err != nil {
 			log.Printf("テーブル %s のエクスポートに失敗: %v\n", table, err)
 		} else {
@@ -84,8 +125,28 @@ func getTables(db *sql.DB, schema string) ([]string, error) {
 	return tables, nil
 }
 
+func getOutputFilePath(outdir, tableName string) (string, error) {
+	// 出力ディレクトリを絶対パスに変換
+	absPath, err := filepath.Abs(outdir)
+	if err != nil {
+		return "", fmt.Errorf("出力ディレクトリのパス取得エラー: %w", err)
+	}
+
+	// ディレクトリが存在しない場合は作成
+	if _, err := os.Stat(absPath); os.IsNotExist(err) {
+		if err := os.MkdirAll(absPath, 0755); err != nil {
+			return "", fmt.Errorf("出力ディレクトリの作成エラー: %w", err)
+		}
+	}
+
+	// ファイルのフルパスを組み立て
+	filePath := filepath.Join(absPath, fmt.Sprintf("%s.csv", tableName))
+
+	return filePath, nil
+}
+
 // テーブルの内容をCSVファイルに書き出す
-func exportTableToCSV(db *sql.DB, schema, table string) error {
+func exportTableToCSV(db *sql.DB, schema, table string, outdir string) error {
 	// クエリを作成
 	query := fmt.Sprintf("SELECT * FROM [%s].[%s]", schema, table)
 	rows, err := db.Query(query)
@@ -101,7 +162,10 @@ func exportTableToCSV(db *sql.DB, schema, table string) error {
 	}
 
 	// CSVファイルを作成
-	fileName := fmt.Sprintf("%s.csv", table)
+	fileName, err := getOutputFilePath(outdir, table)
+	if err != nil {
+		return err
+	}
 	file, err := os.Create(fileName)
 	if err != nil {
 		return err
