56b4c6b489239c214449ff0fa4147eb566285e2d
twinbird <ixa2063@gmail.com>
2025-05-01 17:03:47 +0900

    add test

--- mssql_flag_test.go
+++ mssql_flag_test.go
@@ -69,3 +69,47 @@ func TestSubcommandHelpArgs(t *testing.T) {
 		t.Errorf("call by --help args. want return error, but got nil")
 	}
 }
+
+func TestPasswordFromEnv(t *testing.T) {
+	t.Setenv(DBPukeEnvironmentNamePassword, "saPassword")
+	option, err := parseArgs([]string{
+		"db-puke",
+		"mssql",
+		"-h",
+		"localhost",
+		"-d",
+		"dummy_database",
+		"-s",
+		"dummy_schema",
+		"-u",
+		"sa",
+	}, io.Discard)
+
+	if err != nil {
+		t.Fatalf("call by valid args. want: nil, but got %s", err.Error())
+	}
+
+	if option.DBType != DBTypeMSSql {
+		t.Errorf("option.DBType want: %s, but got %s", DBTypeMSSql, option.DBType)
+	}
+
+	if option.Host != "localhost" {
+		t.Errorf("option.Host want: %s, but got %s", "localhost", option.Host)
+	}
+
+	if option.Database != "dummy_database" {
+		t.Errorf("option.Database want: %s, but got %s", "dummy_database", option.Database)
+	}
+
+	if option.User != "sa" {
+		t.Errorf("option.User want: %s, but got %s", "sa", option.User)
+	}
+
+	if option.Schema != "dummy_schema" {
+		t.Errorf("option.Schema want: %s, but got %s", "dummy_schema", option.Schema)
+	}
+
+	if option.Password != "saPassword" {
+		t.Errorf("option.Password want: %s, but got %s", "saPassword", option.Password)
+	}
+}
